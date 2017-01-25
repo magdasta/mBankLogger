00595dc7fb899be67a4f368c130239f2091a59a0
Magdalena Stasiewicz <magdalena.stasiewicz@kontomatik.com>
2017-01-25 12:07:00 +0100

    improved project structure and added maven dependency

--- src/Runner.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Runner {
-    public static void main(String args[]) throws IOException {
-        MBankLogger mBankLogger = new MBankLogger();
-        mBankLogger.tryIt();
-    }
-}
